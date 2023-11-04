bcda0ea61f9bbf5e6d8f0abbaf96c0545fe61adc
MingliangHong <65794657+MingliangHong@users.noreply.github.com>
2023-11-04 16:50:20 -0700

    Update main_script.py

--- main_script.py
+++ main_script.py
@@ -5,43 +5,44 @@ import io
 
 app = Flask(__name__)
 
-def main_script():
-    def generate():
-        output = io.StringIO()
+def generate():
+    output = io.StringIO()
 
-        # Define an array of graph filenames
-        graph_files = ['graph1.mat', 'graph2.mat', 'graph3.mat', 'graph4.mat', 'graph5.mat', 'graph6.mat']
+    # Define an array of graph filenames
+    graph_files = ['graph1.mat', 'graph2.mat', 'graph3.mat', 'graph4.mat', 'graph5.mat', 'graph6.mat']
 
-        # Iterate through each graph file
-        for idx, graph_file in enumerate(graph_files):
-            # Load the adjacency matrix from the current graph file
-            mat_contents = scipy.io.loadmat(graph_file)
-            # The variable name inside the .mat file is unknown, assuming it's the first key that's not '__globals__', '__header__', or '__version__'
-            variable_names = [name for name in mat_contents if not name.startswith('__')]
-            if variable_names:
-                adj_matrix = mat_contents[variable_names[0]]
-            else:
-                yield f"data: Could not find a valid adjacency matrix in {graph_file}\n\n"
-                continue  # If no valid variable names are found, skip to the next file
+    # Iterate through each graph file
+    for idx, graph_file in enumerate(graph_files):
+        # Load the adjacency matrix from the current graph file
+        mat_contents = scipy.io.loadmat(graph_file)
+        # The variable name inside the .mat file is unknown, assuming it's the first key that's not '__globals__', '__header__', or '__version__'
+        variable_names = [name for name in mat_contents if not name.startswith('__')]
+        if variable_names:
+            adj_matrix = mat_contents[variable_names[0]]
+        else:
+            yield f"data: Could not find a valid adjacency matrix in {graph_file}\n\n"
+            continue  # If no valid variable names are found, skip to the next file
 
-            # Specify the origin node (customizable as needed)
-            origin = 0  # Python uses 0-indexing
+        # Specify the origin node (customizable as needed)
+        origin = 0  # Python uses 0-indexing
 
-            # Call the my_dijkstra function to compute shortest distances
-            dist, prev = my_dijkstra(adj_matrix, origin)
+        # Call the my_dijkstra function to compute shortest distances
+        dist, prev = my_dijkstra(adj_matrix, origin)
 
-            # Write the results to the StringIO buffer
-            output.write(f'Table {idx + 1}: {graph_file}\n')
-            output.write('dist prev\n')
-            for i in range(len(dist)):
-                output.write(f'{dist[i]:<4} {prev[i]:<4}\n')
-            output.write('\n')  # Separate results of different graphs with an empty line
+        # Write the results to the StringIO buffer
+        output.write(f'Table {idx + 1}: {graph_file}\n')
+        output.write('dist prev\n')
+        for i in range(len(dist)):
+            output.write(f'{dist[i]:<4} {prev[i]:<4}\n')
+        output.write('\n')  # Separate results of different graphs with an empty line
 
-            # Yield the current state of the buffer
-            yield f"data: {output.getvalue()}\n\n"
-            output.truncate(0)
-            output.seek(0)
+        # Yield the current state of the buffer
+        yield f"data: {output.getvalue()}\n\n"
+        output.truncate(0)
+        output.seek(0)
 
+@app.route('/stream')
+def stream():
     return Response(stream_with_context(generate()), mimetype="text/event-stream")
 
 @app.route('/')
@@ -54,15 +55,17 @@ def index():
     <meta charset="UTF-8">
     <title>Graph Data</title>
     <script type="text/javascript">
-        var source = new EventSource("/");
+        var source = new EventSource("/stream");
         source.onmessage = function(event) {
-            document.getElementById("result").innerHTML += event.data + "<br>";
+            var dataElement = document.getElementById("result");
+            dataElement.innerHTML += event.data + "<br>";
+            dataElement.scrollTop = dataElement.scrollHeight;
         };
     </script>
     </head>
     <body>
     <h1>Graph Results</h1>
-    <div id="result">
+    <div id="result" style="height: 300px; overflow: auto;">
         <!-- The results will appear here -->
     </div>
     </body>

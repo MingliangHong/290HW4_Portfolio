ab2870a9e7456fed1d8ca7c373ac331035710735
MingliangHong <65794657+MingliangHong@users.noreply.github.com>
2023-11-04 15:48:27 -0700

    Update main_script.py

--- main_script.py
+++ main_script.py
@@ -1,11 +1,16 @@
-
+from flask import Flask
 import scipy.io
 from myDijkstra import my_dijkstra
+import io
+
+app = Flask(__name__)
 
 def main_script():
+    output = io.StringIO()
+
     # Define an array of graph filenames
     graph_files = ['graph1.mat', 'graph2.mat', 'graph3.mat', 'graph4.mat', 'graph5.mat', 'graph6.mat']
-    
+
     # Iterate through each graph file
     for idx, graph_file in enumerate(graph_files):
         # Load the adjacency matrix from the current graph file
@@ -16,19 +21,30 @@ def main_script():
             adj_matrix = mat_contents[variable_names[0]]
         else:
             continue  # If no valid variable names are found, skip to the next file
-        
+
         # Specify the origin node (customizable as needed)
         origin = 0  # Python uses 0-indexing
-        
+
         # Call the my_dijkstra function to compute shortest distances
         dist, prev = my_dijkstra(adj_matrix, origin)
-        
-        # Display the results in a tabular format
-        print(f'Table {idx + 1}: {graph_file}')
-        print('dist prev')
-        for i in range(len(dist)):
-            print(f'{dist[i]:<4} {prev[i]:<4}')
-        print()  # Separate results of different graphs with an empty line
 
-if __name__ == "__main__":
-    main_script()
+        # Write the results to the StringIO buffer
+        output.write(f'Table {idx + 1}: {graph_file}\n')
+        output.write('dist prev\n')
+        for i in range(len(dist)):
+            output.write(f'{dist[i]:<4} {prev[i]:<4}\n')
+        output.write('\n')  # Separate results of different graphs with an empty line
+
+    # Return the entire buffer contents as a string
+    return output.getvalue()
+
+@app.route('/')
+def run_script():
+    # Call main_script and get the output
+    result = main_script()
+    # Return the result to the browser
+    return f"<pre>{result}</pre>"  # Use <pre> tags to format text like in a terminal
+
+if __name__ == '__main__':
+    # Run the Flask app on all available interfaces on port 80
+    app.run(host='0.0.0.0', port=80)

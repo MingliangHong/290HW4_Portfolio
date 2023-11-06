830767f59c733f36bbc5bb4dcd0f06e86d928364
MingliangHong <65794657+MingliangHong@users.noreply.github.com>
2023-11-05 22:15:35 -0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> from flask import Flask
+... 
+... app = Flask(__name__)
+... 
+... @app.route('/')
+... def home():
+...     return 'Hello, Flask!'
+... 
+... if __name__ == '__main__':
+...     app.run(debug=True)

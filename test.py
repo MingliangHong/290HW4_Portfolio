d2f71223da0a0cbca3039f60eff63067f6f287bd
MingliangHong <65794657+MingliangHong@users.noreply.github.com>
2023-11-05 22:23:39 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,8 @@
-Type "help", "copyright", "credits" or "license()" for more information.
->>> from flask import Flask
-... 
-... app = Flask(__name__)
-... 
-... @app.route('/')
-... def home():
-...     return 'Hello, Flask!'
-... 
-... if __name__ == '__main__':
-...     app.run(debug=True)
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def home():
+  return 'Hello, Flask!'
+
+if __name__ == '__main__':
+  app.run(debug=True)
